4467267b38671d0014fbb0ad98f7f5e2aa74c3ac
Vansh Jain <jain.vansh1609@gmail.com>
2024-10-30 21:13:08 +0530

    Refactor app structure: create Quiz, StartScreen, and QuestionsScreen components

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 300,
-                  color: const Color.fromARGB(150, 255, 255, 255),
-                ),
-                const SizedBox(height: 80),
-                const Text(
-                  'Learn Flutter the fun way!',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 30),
-                OutlinedButton.icon(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                  ),
-                  icon: const Icon(Icons.arrow_right_alt),
-                  label: const Text(
-                    'Start Quiz',
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
